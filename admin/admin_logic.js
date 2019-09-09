e5fed8c55f02cce0e30ffc2041b8c9b8642fddf1
Sylvain <sylvduthor@gmail.com>
2019-09-09 16:03:58 +0200

    Sylvain_v3.1

--- admin/admin_logic.js
+++ admin/admin_logic.js
@@ -1,29 +1,65 @@
-function ajoutPizza () {
+$(document).ready(affichePizza());
 
-        var nom = $(".ajout_nom_pizza").val();
-        var image = $(".ajout_image_pizza").val();
-        var description = $(".ajout_description_pizza").val();
-        
-         
-        $.ajax({
-         url: "http://localhost/pizzeria/admin_server.php",
-         type: "POST",
-         data: {
+function ajoutPizza() {
+
+    var nom = $(".ajout_nom_pizza").val();
+    var image = $(".ajout_image_pizza").val();
+    var description = $(".ajout_description_pizza").val();
+
+
+    $.ajax({
+        url: "http://localhost/pizzeria/admin_server.php",
+        type: "POST",
+        data: {
             nom: nom,
             image: image,
             description: description,
-         },
-         success: function success (result) {
-             alert("Pizza ajouté");
-         },
-         error: function error (erreur) {
-             console.log("erreur");
-         }
-     });          
-    
+        },
+        success: function success(result) {
+            alert("Pizza ajouté");
+        },
+        error: function error(erreur) {
+            console.log("erreur");
+        }
+    });
+
 }
 
 
-function liste_pizza () {
-    
+function affichePizza() {
+
+    $.ajax({
+        url: "http://localhost/pizzeria/admin_server.php",
+        type: 'GET',
+        success: function success(result) {
+            result = JSON.parse(result);
+            for (var i = 0; i < result.length; i++) {
+                $(".liste").append(`
+         <div class="liste_pizza">
+            <input type="text" class="liste_nom_pizza" value="` 
+            // Nom pizza
+            + result[i].nom +`">
+        <div class="row_description">
+            <div>
+                <input type="text" class="liste_url_pizza" value="`
+                //url image pizza
+                + result[i].image + `">
+                <input type="text" class="liste_description_pizza" value="`
+                //description pizza
+                + result[i].description + `">
+            </div>
+            //
+             <img src="`
+             //photo pizza
+                + result[i].image + `" class="liste_image_pizza">
+        </div>
+     </div>`);
+            }
+        },
+
+        error: function error(erreur) {
+            console.log(erreur);
+        },
+    });
+
 }
--- pizza/pizza_logic.js
+++ pizza/pizza_logic.js
@@ -1,3 +1,60 @@
+$( document ).ready(affichePizza());
+
+
+function affichePizza() {
+
+   $.ajax({
+       url: "http://localhost/pizzeria/admin_server.php",
+       type: 'GET',
+       success: function success(result) {
+           result = JSON.parse(result);
+           for (var i = 0; i < result.length; i++) {
+               $(".liste").append(`
+               <div class="pizza">
+            <div class="titre_pizza"><span>` 
+            // Nom pizza
+            + result[i].nom +`
+            </span></div>
+            <div class="img_pizza"><u>` 
+            // Nom pizza
+            + result[i].image +`</u>
+            </div>
+            <div class="taille">
+                <span>S</span><input type="radio" name="taille" value="S">
+                <span>M</span><input type="radio" name="taille" value="M">
+                <span>L</span><input type="radio" name="taille" value="L">
+                <span>XL</span><input type="radio" name="taille" value="XL">
+            </div>
+            <div class="nb_pizza">
+                <span>Nombre de pizza : </span>
+                <select class="nom_pizza">
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                    </select>
+            </div>
+            <div>
+                <button class="ajouter" onclick="ajoutPizza()">Ajouter</button>
+            </div>
+        </div>
+               `);
+           }
+       },
+
+       error: function error(erreur) {
+           console.log(erreur);
+       },
+   });
+
+}
+
 function ajoutPizza() {    
    var taille = $('[name="taille"]:checked').val();
    var nb_pizza = $('option:selected').val();
